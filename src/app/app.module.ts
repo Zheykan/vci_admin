b3465a25e5f7a3d17bfcffd5e81157219febba1e
Zheykan <sheik021042@gmail.com>
2024-01-18 01:34:29 -0500

    0.0.2 versión

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './estructura/nav/nav.component';
+import { FooterComponent } from './estructura/footer/footer.component';
+import { ToolbarComponent } from './estructura/toolbar/toolbar.component';
+import { TopbarComponent } from './estructura/topbar/topbar.component';
+import { PrincipalComponent } from './modulos/principal.component';
+import { DashboardComponent } from './modulos/dashboard/dashboard.component';
+import { LoginComponent } from './modulos/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    ToolbarComponent,
+    TopbarComponent,
+    PrincipalComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
